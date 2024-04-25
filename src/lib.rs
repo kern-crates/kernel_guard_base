09e9acd77b5092474330606e857f024e1076cfb8
Shi Lei <shi_lei@massclouds.com>
2024-04-25 20:33:47 +0800

    refine: remove crate_interface

--- src/lib.rs
+++ src/lib.rs
@@ -56,16 +56,6 @@
 
 mod arch;
 
-/// Low-level interfaces that must be implemented by the crate user.
-#[crate_interface::def_interface]
-pub trait KernelGuardIf {
-    /// How to enable kernel preemption.
-    fn enable_preempt();
-
-    /// How to disable kernel preemption.
-    fn disable_preempt();
-}
-
 /// A base trait that all guards implement.
 pub trait BaseGuard {
     /// The saved state when entering the critical section.
@@ -87,27 +77,9 @@ cfg_if::cfg_if! {
     if #[cfg(any(target_os = "none", doc))] {
         /// A guard that disables/enables local IRQs around the critical section.
         pub struct IrqSave(usize);
-
-        /// A guard that disables/enables kernel preemption around the critical
-        /// section.
-        pub struct NoPreempt;
-
-        /// A guard that disables/enables both kernel preemption and local IRQs
-        /// around the critical section.
-        ///
-        /// When entering the critical section, it disables kernel preemption
-        /// first, followed by local IRQs. When leaving the critical section, it
-        /// re-enables local IRQs first, followed by kernel preemption.
-        pub struct NoPreemptIrqSave(usize);
     } else {
         /// Alias of [`NoOp`].
         pub type IrqSave = NoOp;
-
-        /// Alias of [`NoOp`].
-        pub type NoPreempt = NoOp;
-
-        /// Alias of [`NoOp`].
-        pub type NoPreemptIrqSave = NoOp;
     }
 }
 
@@ -147,38 +119,6 @@ mod imp {
         }
     }
 
-    impl BaseGuard for NoPreempt {
-        type State = ();
-        fn acquire() -> Self::State {
-            // disable preempt
-            #[cfg(feature = "preempt")]
-            crate_interface::call_interface!(KernelGuardIf::disable_preempt);
-        }
-        fn release(_state: Self::State) {
-            // enable preempt
-            #[cfg(feature = "preempt")]
-            crate_interface::call_interface!(KernelGuardIf::enable_preempt);
-        }
-    }
-
-    impl BaseGuard for NoPreemptIrqSave {
-        type State = usize;
-        fn acquire() -> Self::State {
-            // disable preempt
-            #[cfg(feature = "preempt")]
-            crate_interface::call_interface!(KernelGuardIf::disable_preempt);
-            // disable IRQs and save IRQ states
-            super::arch::local_irq_save_and_disable()
-        }
-        fn release(state: Self::State) {
-            // restore IRQ states
-            super::arch::local_irq_restore(state);
-            // enable preempt
-            #[cfg(feature = "preempt")]
-            crate_interface::call_interface!(KernelGuardIf::enable_preempt);
-        }
-    }
-
     impl IrqSave {
         /// Creates a new [`IrqSave`] guard.
         pub fn new() -> Self {
@@ -193,45 +133,6 @@ mod imp {
     }
 
     impl Default for IrqSave {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
-
-    impl NoPreempt {
-        /// Creates a new [`NoPreempt`] guard.
-        pub fn new() -> Self {
-            Self::acquire();
-            Self
-        }
-    }
-
-    impl Drop for NoPreempt {
-        fn drop(&mut self) {
-            Self::release(())
-        }
-    }
-
-    impl Default for NoPreempt {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
-
-    impl NoPreemptIrqSave {
-        /// Creates a new [`NoPreemptIrqSave`] guard.
-        pub fn new() -> Self {
-            Self(Self::acquire())
-        }
-    }
-
-    impl Drop for NoPreemptIrqSave {
-        fn drop(&mut self) {
-            Self::release(self.0)
-        }
-    }
-
-    impl Default for NoPreemptIrqSave {
         fn default() -> Self {
             Self::new()
         }
